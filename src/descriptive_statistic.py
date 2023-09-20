17af3e62400fbdc8b9fd0cc426461ae61ad4cfbb
Matthew Holden <matthew.holden86@gmail.com>
2023-09-19 21:32:23 -0400

    added plots and new lib file

--- src/descriptive_statistic.py
+++ src/descriptive_statistic.py
@@ -1,52 +1,25 @@
-'''Returns descriptive statistics for the Iris dataset.'''
+'''Returns descriptive statistics and visualization for the Iris dataset.'''
 
-import os
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-def import_iris(file_name: str):
-    '''Converts csv file to dataframe'''
-
-    if not isinstance(file_name, str):
-        raise TypeError("file_name must be a string")
-    
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-    new_path = parent_dir + "/data"
-    file_path = os.path.join(new_path, file_name)
-
-    try:
-        iris_df = pd.read_csv(file_path)
-        cols = set(iris_df.columns) - {'Id'}
-        reduced_iris = iris_df[list(cols)]
-        return reduced_iris
-    except FileNotFoundError as exc:
-        raise FileNotFoundError(f"The file ({file_name}) was not found.") from exc
-
-def describe_iris(iris_df):
-    '''Returns pandas descriptive statistics on dataframe.'''
-
-    if not isinstance(iris_df, pd.DataFrame):
-        raise TypeError("Iris data must be in pandas dataframe form")
-    iris_stats = iris_df.describe().round(2)
-    # Calculate and add the median manually
-    median = iris_stats.median()
-    iris_stats.loc['50%'] = median
-    iris_stats = iris_stats.rename(index={'50%': 'median'})
-    return iris_stats
-
-def visualize_iris(iris_df):
-    '''Visualize sepal length distribution for each species.'''
-
-    plt.figure(figsize=(10, 6))
-    sns.boxplot(x="Species", y="SepalLengthCm", data=iris_df)
-    plt.title("Boxplot of Sepal Length by Species")
-
-    plt.savefig("SepalLength_by_Species.png")
+import lib
+import warnings
 
     
+def main():
+    '''Main function for descriptive statistics and visualization'''
+
+    warnings.filterwarnings('ignore', category=FutureWarning)
+
+    # Import dataset
+    iris_data = lib.import_iris("iris_data.csv")
+
+    # Print descriptive statistics
+    summary = lib.describe_iris(iris_data)
+    print(summary)
+
+    # Generate and save visualization
+    plot = lib.visualize_iris(iris_data)
+    lib.save_plot("SepalLengthbySpecies_boxplot.png", plot)
+
+
 if __name__ == "__main__":
-    iris_data = import_iris("iris_data.csv")
-    print(describe_iris(iris_data))
-    visualize_iris(iris_data)
+    main()
---
+++ src/lib.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+import seaborn as sns
+
+def import_iris(file_name: str):
+    '''Converts csv file to dataframe'''
+
+    if not isinstance(file_name, str):
+        raise TypeError("file_name must be a string")
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    new_path = parent_dir + "/data"
+    file_path = os.path.join(new_path, file_name)
+
+    try:
+        iris_df = pd.read_csv(file_path)
+        cols = set(iris_df.columns) - {'Id'}
+        reduced_iris = iris_df[list(cols)]
+        return reduced_iris
+    
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(f"The file ({file_name}) was not found.") from exc
+    
+def describe_iris(iris_df):
+    '''Returns pandas descriptive statistics on dataframe.'''
+
+    if not isinstance(iris_df, pd.DataFrame):
+        raise TypeError("Iris data must be in pandas dataframe form")
+    iris_stats = iris_df.describe().round(2)
+    return iris_stats
+
+def visualize_iris(iris_df):
+    '''Visualize sepal length distribution for each species.'''
+
+    g = sns.catplot(
+        x="Species", 
+        y="SepalLengthCm", 
+        data=iris_df,
+        kind="box",
+        height=6, 
+        aspect=1.3
+    )
+    
+    g.set(
+        title="Boxplot of Sepal Length by Species", 
+        xlabel="Species", 
+        ylabel = "Sepal Length (Cm)"
+    )
+    
+    return g
+
+def save_plot(file_name, plot):
+    plot.savefig(
+        "plots/" + f"{file_name}.png"
+    )
